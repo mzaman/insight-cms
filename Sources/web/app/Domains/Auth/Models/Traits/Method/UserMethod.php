adf207e964e5da4d9cb27dbffb3b67bdba13e808
Masud Zaman <masud.zmn@gmail.com>
2025-04-28 08:45:13 +0600

    Safely merge modified files after review

--- Sources/web/app/Domains/Auth/Models/Traits/Method/UserMethod.php
+++ Sources/web/app/Domains/Auth/Models/Traits/Method/UserMethod.php
@@ -100,4 +100,25 @@ trait UserMethod
     {
         return 'https://gravatar.com/avatar/'.md5(strtolower(trim($this->email))).'?s='.config('boilerplate.avatar.size', $size).'&d=mp';
     }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
 }
