4fee5b61f829297c25f44a7c236e06e49c1f0e8c
Masud Zaman <masud.zmn@gmail.com>
2025-05-01 01:49:28 +0600

    Add bindings to App Service Provider

--- Sources/web/app/Domains/V1/Token/Models/Traits/Method/ApiKeyMethod.php
+++ Sources/web/app/Domains/V1/Token/Models/Traits/Method/ApiKeyMethod.php
@@ -8,4 +8,27 @@ namespace App\Domains\V1\Token\Models\Traits\Method;
 trait ApiKeyMethod
 {
   
+    // Method to decrypt the API key, but won't show in the JSON response automatically
+    public function decryptApiKey(): string
+    {
+        if (empty($this->attributes['api_key'])) {
+            throw new \Exception('API key is empty.');
+        }
+
+        return Crypt::decryptString($this->attributes['api_key']);
+    }
+
+    /**
+     * A static method to get the latest decrypted API key
+     */
+    public static function getLatestDecryptedApiKey(): string
+    {
+        $apiKey = self::latest()->first();
+
+        if (!$apiKey) {
+            throw new \Exception('API key not found.');
+        }
+
+        return $apiKey->decryptApiKey(); // Only decrypt when needed
+    }
 }
